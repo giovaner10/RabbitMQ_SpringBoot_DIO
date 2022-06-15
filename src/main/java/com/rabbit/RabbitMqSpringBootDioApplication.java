afbbd481d4fa8b9a883711313989e44e17a73d1c
giovaner10 <giovanerr10@gmail.com>
2022-06-15 15:19:53 -0300

    Initial commit

---
+++ src/main/java/com/rabbit/RabbitMqSpringBootDioApplication.java
@@ -0,0 +1,13 @@
+package com.rabbit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitMqSpringBootDioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitMqSpringBootDioApplication.class, args);
+    }
+
+}
